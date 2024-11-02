86f419ef86f003c6822fd312829ff218c2adc5d8
vvinebrand <vvinebrand@gmail.com>
2024-11-02 12:15:32 +0700

    Добавьте файлы проекта.

---
+++ PR_3_Nekrasova/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PR_3_Nekrasova
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
